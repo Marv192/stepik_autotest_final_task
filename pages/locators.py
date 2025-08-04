d94b60333c92837f19ea4080ff4aeff18c9931f6
Marv192 <marvel.92@inbox.ru>
2025-08-04 17:21:23 +0300

    added product page tests

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
+    ADDED_PRODUCT_NAME =(By.CSS_SELECTOR, ".alert > div > strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main > .price_color")
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".alertinner > p > strong")
---
+++ pages/product_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_to_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_to_basket.click()
+
+    def check_product_name(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        added_product_name = self.browser.find_elements(*ProductPageLocators.ADDED_PRODUCT_NAME)[0].text
+        assert product_name == added_product_name, f"Added product name {added_product_name} doesn't match product name on the page {product_name}"
+
+    def check_basket_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
+        assert product_price == basket_total, f"basket total({basket_total}) is not equal product price({product_price})"
